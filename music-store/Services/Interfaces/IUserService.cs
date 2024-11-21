ee24e0be6f04a6870b1c269d27a943dedb297e39
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2024-11-21 20:22:26 +0300

    Added system users. Closed #9 (#12)

---
+++ music-store/Services/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using music_store.Models.Entities;
+
+namespace music_store.Services.Interfaces
+{
+	public interface IUserService
+	{
+		public bool AddUser(User user);
+	}
+}
---
+++ music-store/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using music_store.Models.Entities;
+using music_store.Services.Interfaces;
+
+namespace music_store.Services
+{
+	public class UserService : IUserService
+	{
+		private ADatabaseConnection _databaseConnection;
+
+		public UserService(ADatabaseConnection aDatabaseConnection)
+		{
+			this._databaseConnection = aDatabaseConnection;
+		}
+
+		public bool AddUser(User user)
+		{
+			try
+			{
+				this._databaseConnection.Users.Add(user);
+				this._databaseConnection.SaveChanges();
+
+				return true;
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.ToString());
+			}
+
+			return false;
+		}
+	}
+}
