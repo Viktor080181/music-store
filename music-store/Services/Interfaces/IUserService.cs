69a668b420be4c056e7b9e7ff1e916eb37c30564
TopAcademyBel <121047497+TopAcademy2022@users.noreply.github.com>
2024-11-21 20:56:06 +0300

    Added method for deleting users. Closed #15 (#17)

--- music-store/Services/Interfaces/IUserService.cs
+++ music-store/Services/Interfaces/IUserService.cs
@@ -5,5 +5,9 @@ namespace music_store.Services.Interfaces
 	public interface IUserService
 	{
 		public bool AddUser(User user);
+
+		public User? FindUser(User user);
+
+		public bool DeleteUser(User user);
 	}
 }
--- music-store/Services/UserService.cs
+++ music-store/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using music_store.Models.Entities;
 using music_store.Services.Interfaces;
 
@@ -17,10 +18,53 @@ namespace music_store.Services
 		{
 			try
 			{
-				this._databaseConnection.Users.Add(user);
-				this._databaseConnection.SaveChanges();
+				User? findedUser = this.FindUser(user);
 
-				return true;
+				if (findedUser == null)
+				{
+					this._databaseConnection.Users.Add(user);
+					this._databaseConnection.SaveChanges();
+
+					return true;
+				}
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.ToString());
+			}
+
+			return false;
+		}
+
+		public User? FindUser(User user)
+		{
+			try
+			{
+				return this._databaseConnection.Users.Where(usr => 
+				usr.Login == user.Login &&
+				usr.Password == user.Password).FirstOrDefault();
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.ToString());
+			}
+
+			return null;
+		}
+
+		public bool DeleteUser(User user)
+		{
+			try
+			{
+				User? findedUser = this.FindUser(user);
+
+				if (findedUser != null)
+				{
+					this._databaseConnection.Users.Remove(findedUser);
+					this._databaseConnection.SaveChanges();
+
+					return true;
+				}
 			}
 			catch (Exception exception)
 			{
