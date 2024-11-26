7279832e6f15381001fc3f0a5d95d092bf377c44
natalia21082000 <161637129+natalia21082000@users.noreply.github.com>
2024-11-26 21:20:03 +0300

    Added method for find vinyl records by name. Closed #23 (#25)

--- music-store/Services/Interfaces/IVinylRecordsService.cs
+++ music-store/Services/Interfaces/IVinylRecordsService.cs
@@ -4,11 +4,20 @@ namespace music_store.Services.Interfaces
 {
 	public interface IVinylRecordsService
 	{
-        /*! 
+		/*! 
 		* @brief Adding plates to our database.
 		* @param[in] vinylRecord - class instance to add.
 		* @return True - vinyl record added; False - vinyl record not added.
 		*/
-        public bool AddVinilRecord(VinylRecord vinylRecord);
+		public bool AddVinilRecord(VinylRecord vinylRecord);
+
+
+	   /*! @fn VinylRecord? SearchByName(string vinylRecordName)
+	   *  @brief Searches for a vinyl record by its name.
+	   *  @param[in] vinylRecordName The name of the vinyl record to search for.
+	   *  @return The first matching vinyl record found, or null if no match is found.
+	   */
+		public VinylRecord? SearchByName(string searchTerm);
+
 	}
 }
--- music-store/Services/VinylRecordsService.cs
+++ music-store/Services/VinylRecordsService.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using System.Linq;
 using music_store.Services.Interfaces;
 using music_store.Models.Entities;
 
 namespace music_store.Services
 {
+	/*! @class VinylRecordsService
+	 *  @brief Service for managing vinyl records in the music store.
+	 */
 	public class VinylRecordsService : IVinylRecordsService
 	{
+		/*! @var _dbConnection
+		 *  @brief Database connection used by the service.
+		 */
 		private ADatabaseConnection _dbConnection; //!< Database connection
 
 		public VinylRecordsService(ADatabaseConnection dbConnection)
@@ -28,6 +35,13 @@ namespace music_store.Services
 			}
 
 			return false;
+		}
+
+		public VinylRecord? SearchByName(string vinylRecordName)
+		{
+			return _dbConnection.VinylRecords
+					   .Where(vr => vr.Name == vinylRecordName)
+					   .FirstOrDefault();
 		}
 	}
 }
