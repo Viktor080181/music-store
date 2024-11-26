cd84072425e2f808088caf6d8bff14cb1706ec28
global133 <161868236+global133@users.noreply.github.com>
2024-11-26 19:47:21 +0300

    Added method for adding music genres records. Closed #6 (#21)

---
+++ music-store/Services/Interfaces/IMusicGenereService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using music_store.Models.Entities;
+
+namespace music_store.Services.Interfaces
+{
+    public interface IMusicGenereService
+    {
+        /*! 
+		* @brief Adding genres to our database.
+        * @param[in] MusicGenre genere - class instance to add.
+        * @return True - music genere added; False - music genere not added.
+        */
+        public bool AddMusicGenre(MusicGenre genere);
+    }
+}
---
+++ music-store/Services/MusicGenereService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using music_store.Models.Entities;
+using music_store.Services.Interfaces;
+
+namespace music_store.Services
+{
+    public class MusicGenereService : IMusicGenereService
+    {
+        private ADatabaseConnection _databaseConnection;
+
+        public MusicGenereService(ADatabaseConnection databaseConnection)
+        {
+            this._DatabaseConnection = databaseConnection;
+        }
+        
+        public bool AddMusicGenre(MusicGenre genere)
+        {
+            try
+            {
+                this._DatabaseConnection.MusicGenres.Add(genere);
+                this._DatabaseConnection.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return false;
+            }
+        }
+    }
+}
